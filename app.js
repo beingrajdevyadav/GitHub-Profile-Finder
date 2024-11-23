5f333ab72c48c075f86cd1faccf4a295fc0db795
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-11-23 20:50:00 +0530

    javascript advanced features added.

--- app.js
+++ app.js
@@ -11,10 +11,15 @@ async function fetchProfile(username) {
         }
 
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
+        // console.log(formatISODate(data.created_at));
 
-        console.log(formatISODate(data.created_at));
-
+        if (document.getElementById("loader-01")) {
+            document.getElementById("loader-01").style.display = "none";
+            printGitHubProfile(data);
+        } else {
+            printGitHubProfile(data);
+        }
 
         fetchRepo(data.login);
     } catch (error) {
@@ -36,6 +41,12 @@ async function fetchRepo(username) {
 
         const data = await response.json();
         // console.log(data);
+        if (document.getElementById("loader-02")) {
+            document.getElementById("loader-02").style.display = "none";
+            printGitHubRepository(data);
+        } else {
+            printGitHubRepository(data);
+        }
     } catch (error) {
         console.error('Error fetching data : ', error);
     }
@@ -62,7 +73,7 @@ function formatISODate(isoString) {
 // ---->>>>----->>>>>------->>>>>------>>>>>------->>>>
 //           Number Formator Function
 // ---->>>>----->>>>>------->>>>>------>>>>>------->>>>
-function formatNumber(num){
+function formatNumber(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
@@ -76,3 +87,95 @@ const searchBtn = document.querySelector("#search-btn");
 searchBtn.addEventListener("click", function () {
     fetchProfile(username.value);
 })
+
+// ---->>>>----->>>>>------->>>>>------>>>>>------->>>>
+//      function to print github profile
+// ---->>>>----->>>>>------->>>>>------>>>>>------->>>>
+function printGitHubProfile(data) {
+    const currentProfile = `
+     <div class="profile-img">
+                <img src="${data.avatar_url}" alt="profile-img">
+            </div>
+
+            <div class="p-1">
+                <h2 class="profile-name txt-center">${data.name}</h2>
+                <p class="profile-bio txt-center">${data.bio}</p>
+            </div>
+
+            <hr>
+            <div class="profile-fame txt-center">
+                <div class="fame">
+                    <h1>${formatNumber(data.followers)}</h1>
+                    <p>Followers</p>
+                </div>
+                <div class="fame">
+                    <h1>${formatNumber(data.following)}</h1>
+                    <p>Following</p>
+                </div>
+                <div class="fame">
+                    <h1>${formatNumber(data.public_repos)}</h1>
+                    <p>Repository</p>
+                </div>
+            </div>
+
+            <div class="profile-info">
+                <div class="info-item">
+                    <div class="info-item-icon">
+                        <i class="fa-solid fa-location-dot"></i> <span> Location :</span>
+                    </div>
+                    <p> ${data.location}</p>
+                </div>
+                <div class="info-item">
+                    <div class="info-item-icon">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <span>Company :</span>
+                    </div>
+                    <p>${data.company}</p>
+                </div>
+                <div class="info-item">
+                    <div class="info-item-icon">
+                        <i class="fa-solid fa-at"></i>
+                        <span>GitHub URL :</span>
+                    </div>
+                    <p><a href="${data.html_url}" target="_blank">Open Link</a></p>
+                </div>
+            </div>
+            <hr>
+
+            <div class="timing">
+                <p>Joined : ${formatISODate(data.created_at)}</p>
+                <p>Last Update : ${formatISODate(data.updated_at)}</p>
+            </div>
+    `;
+
+    document.getElementById("user-profile").innerHTML = currentProfile;
+};
+
+// ---->>>>----->>>>>------->>>>>------>>>>>------->>>>
+//      function to print github repository
+// ---->>>>----->>>>>------->>>>>------>>>>>------->>>>
+function printGitHubRepository(repos) {
+
+    let reposList = '';
+    repos.forEach(repo => {
+        reposList += `
+      <div class="repository">
+  
+                     <h2>${repo.name}</h2>
+  
+                      <div class="repo-info">
+                          <p>Language : ${repo.language}</p>
+                          <p>GitHub Link : <a href="${repo.html_url}" target="_blank">Open Link</a></p>
+                      </div>
+                      <hr>
+                      <div class="repo-timing">
+                          <p>Created : ${formatISODate(repo.created_at)} </p>
+                          <p>Last Update : ${formatISODate(repo.updated_at)} </p>
+                      </div>
+                  </div>
+      `;
+    });
+
+    document.getElementById("reposList").innerHTML = reposList;
+    document.getElementById("total-repos").innerHTML = repos.length;
+}
